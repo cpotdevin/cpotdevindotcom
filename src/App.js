650121bdfb02ae32aac3c31656daace8b2773d2a
cpotdevin <chrpotdevin@gmail.com>
2019-01-07 23:06:46 -0500

    update menu

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+import Routes from './Routes';
+import './index.css';
+
+const App = () => (
+  <div>
+    <nav className='header'>
+      <div className='header-cont'>
+        <h1 className='logo'><NavLink exact to='/'>cpotdevin.com</NavLink></h1>
+        <ul className='menu'>
+          <li><NavLink exact to='/'>home</NavLink></li>
+          <li><NavLink exact to='/proyects'>proyects</NavLink></li>
+        </ul>
+      </div>
+    </nav>
+    <Routes />
+  </div>
+);
+
+export default App;
