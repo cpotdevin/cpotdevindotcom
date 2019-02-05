6a4bbfa175ce8fbe047e63408c1ed0b630c4ab9d
cpotdevin <chrpotdevin@gmail.com>
2019-02-05 08:39:26 -0500

    correct NavBar container and items

--- app/src/NavBar.js
+++ app/src/NavBar.js
@@ -4,11 +4,11 @@ import {NavLink} from 'react-router-dom';
 import './NavBar.css';
 
 const NavBar = () => (
-  <Grid container className='header' direction='row' justify='center' alignItems='center'>
-    <Grid container className='header-cont' sx={12} md={5}>
+  <Grid container className='header' justify='center' alignItems='center'>
+    <Grid item className='header-cont' xs={12} md={5}>
       <h1 className='logo'><NavLink exact to='/'>cpotdevin.com</NavLink></h1>
     </Grid>
-    <Grid container className='menu-container' sx={12} md={5}>
+    <Grid item className='menu-container' xs={12} md={5}>
       <ul className='menu'>
         <li><NavLink exact to='/'>home</NavLink></li>
         <li><NavLink exact to='/about-me'>about me</NavLink></li>
