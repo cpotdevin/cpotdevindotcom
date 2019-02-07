935d40ded90099d20b456d69295c67f06b3311ec
cpotdevin <chrpotdevin@gmail.com>
2019-02-07 13:20:31 -0500

    fix styling

--- app/src/content-helpers/Video.js
+++ app/src/content-helpers/Video.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Video = ({width, height, src}) => (
-  <iframe width={width} height={height} src={src} title={src} frameBorder='0' allow="autoplay; encrypted-media; picture-in-picture" allowFullScreen />
+  <iframe className='video' width={width} height={height} src={src} title={src} frameBorder='0' allow="autoplay; encrypted-media; picture-in-picture" allowFullScreen />
 );
 
 export default Video;
