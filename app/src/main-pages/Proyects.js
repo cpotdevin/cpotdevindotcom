2b07e304980350ebbd0fcb2dcbbba3f89ad8d196
cpotdevin <chrpotdevin@gmail.com>
2019-02-07 15:51:51 -0500

    add proyect descriptions

--- app/src/main-pages/Proyects.js
+++ app/src/main-pages/Proyects.js
@@ -35,6 +35,16 @@ export default class Proyects extends React.Component {
         </Grid> */}
         <Grid container direction='row' justify='center' alignContent='center'>
           <Grid item xs={10} md={8}>
+            <h3>F1 Race Game Programmed on RTOS</h3>
+            <p>This is a recreation of the F1 Race game for the Game Boy from 1990. It was programmed on the STM32F4DISCOVERY kit using C and the real time operating system FreeRTOS. Challenges included maintaining a constant 50 frames per second, having smooth animations and constantly reading user input. Afterwards, a multiplayer system was implemented in which another board could be connected using the UART protocol.</p>
+          </Grid>
+          <Grid item className='test' xs={10} md={8}>
+            <VideoContainer width='315' height='560' src="https://www.youtube-nocookie.com/embed/_xuQujn2-LE" />
+          </Grid>
+        </Grid>
+
+        <Grid container direction='row' justify='center' alignContent='center'>
+          <Grid item xs={10} md={8}>
             <h3>Mirror Spheres</h3>
             <p>The following images were made using a 360˚ camera, Cinema4D and Adobe Photoshop.</p>
           </Grid>
@@ -50,18 +60,8 @@ export default class Proyects extends React.Component {
 
         <Grid container direction='row' justify='center' alignContent='center'>
           <Grid item xs={10} md={8}>
-            <h3>F1 Race Game Programmed on RTOS</h3>
-            <p></p>
-          </Grid>
-          <Grid item className='test' xs={10} md={8}>
-            <VideoContainer width='315' height='560' src="https://www.youtube-nocookie.com/embed/_xuQujn2-LE" />
-          </Grid>
-        </Grid>
-
-        <Grid container direction='row' justify='center' alignContent='center'>
-          <Grid item xs={10} md={8}>
             <h3>3x3x3 LED Cube</h3>
-            <p></p>
+            <p>This lamp was designed and made by me. It was programmed using the Arduino IDE and using components such as the ATMega328P, the 2N2222 NPN transistor and the 74HC595 8-bit shift register. Further information on code and schematics can be foundo over at <a href='https://github.com/cpotdevin/3x3x3_LED_Cube'>my github page</a>.</p>
           </Grid>
           <Grid item className='test' xs={10} md={8}>
             <VideoContainer width='315' height='560' src="https://www.youtube-nocookie.com/embed/GTOnFk35C70" />
@@ -71,7 +71,7 @@ export default class Proyects extends React.Component {
         <Grid container direction='row' justify='center' alignContent='center'>
           <Grid item xs={10} md={8}>
             <h3>Robot Golf Ball Collector</h3>
-            <p></p>
+            <p>The following video shows a bluetooth controlled robot that collects golf balls from a tree like structure. The robot was programmed using the Arduino IDE and all parts where lazer cut on plywood. Challenges included maintaining certain size and weight restrictions while being able to sort the golf balls by color.</p>
           </Grid>
           <Grid item className='test' xs={10} md={8}>
             <VideoContainer width='560' height='315' src="https://www.youtube-nocookie.com/embed/7q8TOWlyzqU" />
@@ -81,7 +81,7 @@ export default class Proyects extends React.Component {
         <Grid container direction='row' justify='center' alignContent='center'>
           <Grid item xs={10} md={8}>
             <h3>3D Animation</h3>
-            <p></p>
+            <p>This animation was done using Cinema4D.</p>
           </Grid>
           <Grid item className='test' xs={10} md={8}>
             <VideoContainer width='560' height='315' src="https://www.youtube-nocookie.com/embed/MLxleatB2IA" />
