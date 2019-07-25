5bfc2449c7efe697919109dabd64982bc7ae1191
cpotdevin <chrpotdevin@gmail.com>
2019-07-24 20:35:58 -0500

    feat: add index html and css files

--- app/src/content-helpers/Video.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Video = ({width, height, src}) => (
-  <iframe className='video' width={width} height={height} src={src} title={src} frameBorder='0' allow="autoplay; encrypted-media; picture-in-picture" allowFullScreen />
-);
-
-export default Video;
